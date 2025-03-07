1a7ebe48696566808f3aaeeafae21466e66b3cd1
Mukesh-16 <mukeshbairu41655@gmail.com>
2025-03-07 17:11:46 -0500

    Model Training Added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.ML_Project import logger
 from ML_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ML_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from ML_Project.pipeline.stage_03_data_transformation import DataTransformationPipeline
+from ML_Project.pipeline.stage_04_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "DATA INGESTION"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
@@ -14,8 +15,8 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-STAGE_NAME = "DATA VALIDATION"
 
+STAGE_NAME = "DATA VALIDATION"
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -23,6 +24,30 @@ if __name__ == '__main__':
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
         
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "DATA TRANSFORMATION"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "Model Training"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
